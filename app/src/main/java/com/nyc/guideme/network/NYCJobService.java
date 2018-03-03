b68635cda19d6a9c4f55327c58ad0b6793085ef2
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-03-03 13:41:22 -0500

    retrofitclient has medicaid and jobs

--- app/src/main/java/com/nyc/guideme/network/NYCJobService.java
+++ app/src/main/java/com/nyc/guideme/network/NYCJobService.java
@@ -3,6 +3,8 @@ package com.nyc.guideme.network;
 import com.nyc.guideme.models.JobModels;
 import com.nyc.guideme.models.MedicaidModels;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -13,8 +15,8 @@ import retrofit2.http.GET;
 public interface NYCJobService {
 
     @GET("resource/swhp-yxa4.json")
-    Call<JobModels> getJobsModels();
+    Call<List<JobModels>> getJobsModels();
 
     @GET("resource/vydn-n796.json")
-    Call<MedicaidModels> getMedicaidModels();
+    Call<List<MedicaidModels>> getMedicaidModels();
 }
