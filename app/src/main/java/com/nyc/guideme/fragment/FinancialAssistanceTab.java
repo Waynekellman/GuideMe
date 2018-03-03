4288cb067dea8128db7c965194b2fa7cb6ee630c
CasablancaC4Q <32021306+CasablancaC4Q@users.noreply.github.com>
2018-03-03 15:55:59 -0500

    jobs itemview

--- app/src/main/java/com/nyc/guideme/fragment/FinancialAssistanceTab.java
+++ app/src/main/java/com/nyc/guideme/fragment/FinancialAssistanceTab.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +21,8 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class FinancialAssistanceTab extends Fragment {
+
+      // private RecyclerView recyclerView ;
 
     private static final String TAG = "FinancialAssistanceTab";
     private OnFragmentInteractionListener mListener;
