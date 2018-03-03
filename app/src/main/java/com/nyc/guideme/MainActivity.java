65474ee0eadc7a315c200ada461d6e71b54c8c57
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2018-03-03 13:15:20 -0500

    viewPage done

--- app/src/main/java/com/nyc/guideme/MainActivity.java
+++ app/src/main/java/com/nyc/guideme/MainActivity.java
@@ -1,13 +1,52 @@
 package com.nyc.guideme;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.nyc.guideme.fragment.FinancialAssistanceTab;
+import com.nyc.guideme.fragment.JobTab;
+import com.nyc.guideme.fragment.MedicalAssistanceTab;
+
+public class MainActivity extends AppCompatActivity implements JobTab.OnFragmentInteractionListener, FinancialAssistanceTab.OnFragmentInteractionListener, MedicalAssistanceTab.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TabLayout tabLayout = findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Job"));
+        tabLayout.addTab(tabLayout.newTab().setText("FinancialAssistance"));
+        tabLayout.addTab(tabLayout.newTab().setText("MedicalAssistance"));
+        final ViewPager pager = findViewById(R.id.pager);
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        pager.setAdapter(pageAdapter);
+        pager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tabLayout.getSelectedTabPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/nyc/guideme/fragment/JobTab.java
@@ -0,0 +1,35 @@
+package com.nyc.guideme.fragment;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nyc.guideme.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JobTab extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    public JobTab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_job_tab, container, false);
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
