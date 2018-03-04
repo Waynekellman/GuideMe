6f1d560112c776d376c86ea725daea763929d6f0
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-03-03 22:15:20 -0500

    the list passes into JobsDetailsActivity

--- app/src/main/java/com/nyc/guideme/JobDetailsActivity.java
+++ app/src/main/java/com/nyc/guideme/JobDetailsActivity.java
@@ -6,27 +6,32 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.nyc.guideme.adapter.SwipeStackAdapter;
 import com.nyc.guideme.models.JobModels;
+import com.nyc.guideme.models.JobsListSingleton;
+import com.nyc.guideme.network.RetrofitClient;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import link.fls.swipestack.SwipeStack;
 
-public class JobDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class JobDetailsActivity extends AppCompatActivity {
 
-    private JobModels job;
+    private static final String TAG = "JobDetailsActivity";
+    public List<JobModels> jobModelsList;
     private TextView businessTitle, civicTitle, divisionWorkUnit,
             salaryFrom, salaryTo, salaryFrequency, jobCategory,
             workLocation, fpIndicator, jobDescription, minimumQualification,
@@ -44,22 +49,17 @@ public class JobDetailsActivity extends AppCompatActivity implements OnMapReadyC
         sharedPreferences = getSharedPreferences(SHARED_PREF_KEY, MODE_PRIVATE);
         SwipeStack swipeStack = (SwipeStack) findViewById(R.id.swipeStack);
 
-
-        Intent intent = getIntent();
-        if (intent.hasExtra("jobDetails")) {
-            job = new Gson().fromJson(intent.getStringExtra("jobDetails"), JobModels.class);
-            List<JobModels> jobModelsTest=new ArrayList<>();
-            jobModelsTest.add(job);
-            swipeStack.setAdapter(new SwipeStackAdapter(jobModelsTest));
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.clear();
-            editor.putString("jobDetails",intent.getStringExtra("jobDetails") );
-        }
-
-        if (!sharedPreferences.getString("jobDetails", "").equals("")){
-            job = new Gson().fromJson(sharedPreferences.getString("jobDetails", ""), JobModels.class);
-        }
-
+//        Type type = new TypeToken<List<JobModels>>() {
+//        }.getType();
+//        Intent intent = getIntent();
+//        ArrayList<String> jobsStringArray = intent.getStringArrayListExtra("jobDetails");
+//        for (String s : jobsStringArray) {
+//            jobModelsList.add(new Gson().fromJson(s, JobModels.class));
+//        }
+        jobModelsList = JobsListSingleton.getInstance().getJobModelsArrayList();
+        Log.d(TAG, "onCreate: " + jobModelsList.get(0).getBusiness_title());
+        Log.d(TAG, "onCreate: " + String.valueOf(jobModelsList.size()));
+        swipeStack.setAdapter(new SwipeStackAdapter(jobModelsList));
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         /*
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -72,40 +72,40 @@ public class JobDetailsActivity extends AppCompatActivity implements OnMapReadyC
 //        setViews();
     }
 
-    private void setViews() {
-        businessTitle.setText(job.getBusiness_title());
-        civicTitle.setText(job.getCivil_service_title());
-        divisionWorkUnit.setText(job.getDivision_work_unit());
-        salaryFrom.setText(job.getSalary_range_from());
-        salaryTo.setText(job.getSalary_range_to());
-        salaryFrequency.setText(job.getSalary_frequency());
-        jobCategory.setText(job.getJob_category());
-        workLocation.setText(job.getWork_location());
-        fpIndicator.setText(job.getFull_time_part_time_indicator());
-        minimumQualification.setText(job.getMinimum_qual_requirements());
-        residencyReq.setText(job.getResidency_requirement());
-        postUntil.setText(job.getPost_until());
-        postingDate.setText(job.getPosting_date());
-        preferredSkills.setText(job.getPreferred_skills());
-    }
-
-    private void initViews() {
-        businessTitle = findViewById(R.id.business_title_details);
-        civicTitle = findViewById(R.id.civic_title_details);
-        divisionWorkUnit = findViewById(R.id.division_work_unit_details);
-        salaryFrom = findViewById(R.id.salary_from_details);
-        salaryTo = findViewById(R.id.salary_to_details);
-        salaryFrequency = findViewById(R.id.salary_frequency_detail);
-        jobCategory = findViewById(R.id.job_category_details);
-        workLocation = findViewById(R.id.work_location_details);
-        fpIndicator = findViewById(R.id.full_part_indicator_details);
-        jobDescription = findViewById(R.id.job_description_details);
-        minimumQualification = findViewById(R.id.minimum_req_details);
-        residencyReq = findViewById(R.id.residency_req_details);
-        postUntil = findViewById(R.id.post_until_details);
-        postingDate = findViewById(R.id.post_date_details);
-        preferredSkills = findViewById(R.id.prefered_skills_details);
-    }
+//    private void setViews() {
+//        businessTitle.setText(jobModelsList.getBusiness_title());
+//        civicTitle.setText(jobModelsList.getCivil_service_title());
+//        divisionWorkUnit.setText(jobModelsList.getDivision_work_unit());
+//        salaryFrom.setText(jobModelsList.getSalary_range_from());
+//        salaryTo.setText(jobModelsList.getSalary_range_to());
+//        salaryFrequency.setText(jobModelsList.getSalary_frequency());
+//        jobCategory.setText(jobModelsList.getJob_category());
+//        workLocation.setText(jobModelsList.getWork_location());
+//        fpIndicator.setText(jobModelsList.getFull_time_part_time_indicator());
+//        minimumQualification.setText(jobModelsList.getMinimum_qual_requirements());
+//        residencyReq.setText(jobModelsList.getResidency_requirement());
+//        postUntil.setText(jobModelsList.getPost_until());
+//        postingDate.setText(jobModelsList.getPosting_date());
+//        preferredSkills.setText(jobModelsList.getPreferred_skills());
+//    }
+//
+//    private void initViews() {
+//        businessTitle = findViewById(R.id.business_title_details);
+//        civicTitle = findViewById(R.id.civic_title_details);
+//        divisionWorkUnit = findViewById(R.id.division_work_unit_details);
+//        salaryFrom = findViewById(R.id.salary_from_details);
+//        salaryTo = findViewById(R.id.salary_to_details);
+//        salaryFrequency = findViewById(R.id.salary_frequency_detail);
+//        jobCategory = findViewById(R.id.job_category_details);
+//        workLocation = findViewById(R.id.work_location_details);
+//        fpIndicator = findViewById(R.id.full_part_indicator_details);
+//        jobDescription = findViewById(R.id.job_description_details);
+//        minimumQualification = findViewById(R.id.minimum_req_details);
+//        residencyReq = findViewById(R.id.residency_req_details);
+//        postUntil = findViewById(R.id.post_until_details);
+//        postingDate = findViewById(R.id.post_date_details);
+//        preferredSkills = findViewById(R.id.prefered_skills_details);
+//    }
 
     public Address getLocationFromAddress(String strAddress) throws IOException {
 
@@ -122,19 +122,19 @@ public class JobDetailsActivity extends AppCompatActivity implements OnMapReadyC
 
         return location;
     }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        mMap = googleMap;
-        try {
-            location = getLocationFromAddress(job.getWork_location());
-            LatLng jobMarker = new LatLng(location.getLatitude(),location.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(jobMarker).title(job.getAgency()));
-            float zoomLevel = (float) 14.0; //This goes up to 21
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jobMarker, zoomLevel));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//
+//        mMap = googleMap;
+//        try {
+//            location = getLocationFromAddress(jobModelsList.getWork_location());
+//            LatLng jobMarker = new LatLng(location.getLatitude(),location.getLongitude());
+//            mMap.addMarker(new MarkerOptions().position(jobMarker).title(jobModelsList.getAgency()));
+//            float zoomLevel = (float) 14.0; //This goes up to 21
+//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jobMarker, zoomLevel));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
--- app/src/main/java/com/nyc/guideme/adapter/JobAdapter.java
+++ app/src/main/java/com/nyc/guideme/adapter/JobAdapter.java
@@ -1,16 +1,24 @@
 package com.nyc.guideme.adapter;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.nyc.guideme.JobDetailsActivity;
 import com.nyc.guideme.R;
 import com.nyc.guideme.models.JobModels;
+import com.nyc.guideme.models.JobsListSingleton;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +30,7 @@ public class JobAdapter extends RecyclerView.Adapter<JobViewHoler> {
 
 
     private List<JobModels> jobList = new ArrayList<>();
+    private final String TAG = "JobAdapter";
 
     public JobAdapter(List<JobModels> jobList) {
         this.jobList = jobList;
@@ -39,9 +48,16 @@ public class JobAdapter extends RecyclerView.Adapter<JobViewHoler> {
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String jobJSON = new Gson().toJson(jobList.get(position));
+                String jobListJSON = new Gson().toJson(jobList);
                 Intent intent = new Intent(holder.itemView.getContext(), JobDetailsActivity.class);
-                intent.putExtra("jobDetails", jobJSON);
+                Log.d(TAG, "onClick: " + jobListJSON);
+                JobsListSingleton.getInstance().setJobModelsArrayList(jobList);
+//                ArrayList<String> jobsStringArray = new ArrayList<>();
+//                for (JobModels j : jobList) {
+//                    jobsStringArray.add(new Gson().toJson(j));
+//                }
+////                intent.putExtra("jobDetails", jobListJSON);
+//                intent.putStringArrayListExtra("jobDetails",jobsStringArray);
                 holder.itemView.getContext().startActivity(intent);
             }
         });
