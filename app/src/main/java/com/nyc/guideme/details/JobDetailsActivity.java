147ef532582765fa7fadce4728f7cd307a4933bd
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2018-03-04 15:11:36 -0500

    fix the swap

--- app/src/main/java/com/nyc/guideme/details/JobDetailsActivity.java
+++ app/src/main/java/com/nyc/guideme/details/JobDetailsActivity.java
@@ -1,32 +1,70 @@
 package com.nyc.guideme.details;
 
-import android.content.SharedPreferences;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.maps.GoogleMap;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Filter;
+import android.widget.Toast;
+
 import com.nyc.guideme.adapter.SwipeStackAdapter;
 import com.nyc.guideme.R;
 import com.nyc.guideme.models.JobModels;
 import com.nyc.guideme.models.JobsListSingleton;
+
 import java.io.IOException;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import link.fls.swipestack.SwipeStack;
 
 public class JobDetailsActivity extends AppCompatActivity {
 
     private static final String TAG = "JobDetailsActivity";
+    private CircleImageView circleImageView;
     public List<JobModels> jobModelsList;
+    private SwipeStack swipeStack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.job_details_card);
-        SwipeStack swipeStack = (SwipeStack) findViewById(R.id.swipeStack);
+        circleImageView = findViewById(R.id.circle_id);
+        circleImageView.setVisibility(View.INVISIBLE);
+        swipeStack = (SwipeStack) findViewById(R.id.swipeStack);
+
+
+        swipeStack.setSwipeProgressListener(new SwipeStack.SwipeProgressListener() {
+            @Override
+            public void onSwipeStart(int position) {
+
+            }
+
+            @Override
+            public void onSwipeProgress(int position, float progress) {
+
+                if (progress >= 0) {
+                    circleImageView.setVisibility(View.VISIBLE);
+                    circleImageView.setBackground(getResources().getDrawable(R.drawable.applynow));
+                } else if (progress < 0) {
+                    circleImageView.setVisibility(View.VISIBLE);
+                    circleImageView.setBackground(getResources().getDrawable(R.drawable.dismiss));
+                }
+
+            }
+
+            @Override
+            public void onSwipeEnd(int position) {
+                circleImageView.setVisibility(View.INVISIBLE);
+            }
+        });
+
         jobModelsList = JobsListSingleton.getInstance().getJobModelsArrayList();
         Log.d(TAG, "onCreate: " + jobModelsList.get(0).getBusiness_title());
         Log.d(TAG, "onCreate: " + String.valueOf(jobModelsList.size()));
@@ -48,4 +86,6 @@ public class JobDetailsActivity extends AppCompatActivity {
 
         return location;
     }
+
+
 }
